2d6b12dc24debb4679a756669eab618f44be3ca5
Vali-Hameed <valihameed88@gmail.com>
2025-09-19 21:54:40 +0100

    fixed atomic transaction error

--- src/main/java/com/valihameed/ufcfightpredictor/registration/RegistrationService.java
+++ src/main/java/com/valihameed/ufcfightpredictor/registration/RegistrationService.java
@@ -4,8 +4,10 @@ import com.valihameed.ufcfightpredictor.repository.roleRepository;
 import com.valihameed.ufcfightpredictor.users.role;
 import com.valihameed.ufcfightpredictor.users.user;
 import com.valihameed.ufcfightpredictor.users.userService;
+
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @AllArgsConstructor
@@ -13,6 +15,8 @@ public class RegistrationService {
     private final userService userService;
     private final emailValidator emailValidator;
     private final roleRepository roleRepository;
+
+    @Transactional
     public String register(RegistrationRequest request) {
         boolean isValidEmail = emailValidator.test(request.getEmail());
         if (!isValidEmail) {
