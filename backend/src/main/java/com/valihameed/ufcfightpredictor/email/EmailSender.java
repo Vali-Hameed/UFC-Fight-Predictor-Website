4c454ac9fe02966b3dba96e48ed498c79a433734
Vali-Hameed <valihameed88@gmail.com>
2026-06-02 19:49:55 +0100

    feat: update email templates and sender service

--- backend/src/main/java/com/valihameed/ufcfightpredictor/email/EmailSender.java
+++ backend/src/main/java/com/valihameed/ufcfightpredictor/email/EmailSender.java
@@ -1,5 +1,5 @@
 package com.valihameed.ufcfightpredictor.email;
 
 public interface EmailSender {
-    void sendEmail(String to,String email);
+    void sendEmail(String to, String email, String subject);
 }
--- backend/src/main/java/com/valihameed/ufcfightpredictor/email/EmailService.java
+++ backend/src/main/java/com/valihameed/ufcfightpredictor/email/EmailService.java
@@ -20,14 +20,14 @@ public class EmailService implements EmailSender {
     private final JavaMailSender mailSender;
     @Override
     @Async
-    public void sendEmail(String to, String email) {
+    public void sendEmail(String to, String email, String subject) {
         try{
             MimeMessage mimeMessage = mailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
             helper.setText(email, true);
             helper.setTo(to);
             helper.setFrom("UFFightPredictor@UFCFightPredictor.com");
-            helper.setSubject("Confirm your email");
+            helper.setSubject(subject);
             mailSender.send(mimeMessage);
 
         }catch (MessagingException e){
