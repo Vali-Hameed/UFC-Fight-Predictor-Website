9e82e3558a3abf8fcda94254d22addb0c72df156
Vali-Hameed <valihameed88@gmail.com>
2026-06-07 23:31:45 +0100

    Fix package name casing in FighterController

--- backend/src/main/java/com/valihameed/ufcfightpredictor/controller/FighterController.java
+++ backend/src/main/java/com/valihameed/ufcfightpredictor/controller/FighterController.java
@@ -1,4 +1,4 @@
-package com.valihameed.UFCFightPredictor.controller;
+package com.valihameed.ufcfightpredictor.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
