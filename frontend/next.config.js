63247b92decb03107a3d37b42edcd6546c8daf4b
Vali-Hameed <valihameed88@gmail.com>
2026-06-10 16:44:05 +0100

    Update next.config.js to support dynamic backend URL

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,10 +2,12 @@
 const nextConfig = {
   typedRoutes: true,
   async rewrites() {
+    // Vercel will use BACKEND_URL if you set it in your Vercel Environment Variables
+    const backendUrl = process.env.BACKEND_URL || 'http://79.72.93.231:8080';
     return [
       {
         source: '/api/:path*',
-        destination: 'http://79.72.93.231:8080/api/:path*',
+        destination: `${backendUrl}/api/:path*`,
       },
     ]
   },
